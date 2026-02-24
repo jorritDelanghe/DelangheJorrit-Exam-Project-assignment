e7714e61c9154d0efd7ab690059408a8e1eda243
jorritDelanghe <157893404+jorritDelanghe@users.noreply.github.com>
2026-02-24 15:36:15 +0100

    remove objects
    created a std::vector with objects marked delete so that they can safely be deleted in the update

--- Minigin/Scene.cpp
+++ Minigin/Scene.cpp
@@ -12,27 +12,35 @@ void Scene::Add(std::unique_ptr<GameObject> object)
 
 void Scene::Remove(const GameObject& object)
 {
-	m_objects.erase(
-		std::remove_if(
-			m_objects.begin(),
-			m_objects.end(),
-			[&object](const auto& ptr) { return ptr.get() == &object; }
-		),
-		m_objects.end()
-	);
+	m_pendingDeleteObjects.emplace_back(&object);
 }
 
 void Scene::RemoveAll()
 {
 	m_objects.clear();
+	m_pendingDeleteObjects.clear();
 }
 
 void Scene::Update(float deltaTime)
 {
-	for(auto& object : m_objects)
+	for (auto& object : m_objects)
 	{
 		object->Update(deltaTime);
 	}
+
+	m_objects.erase(
+	std::remove_if(m_objects.begin(), m_objects.end()
+		,[&](const auto& obj)
+		{ 
+			return std::find(
+			m_pendingDeleteObjects.begin()
+			,m_pendingDeleteObjects.end()
+			, obj.get()) != m_pendingDeleteObjects.end(); 
+		})
+		, m_objects.end()
+	);	
+
+	m_pendingDeleteObjects.clear();
 }
 
 void Scene::Render() const
