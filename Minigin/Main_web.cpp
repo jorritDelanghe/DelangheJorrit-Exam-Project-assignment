799eaed9a6a11b6a5126d105fb91158f7e6a1430
jorritDelanghe <157893404+jorritDelanghe@users.noreply.github.com>
2026-03-11 22:00:48 +0100

    emscipting save

---
+++ Minigin/Main_web.cpp
@@ -0,0 +1,151 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+#if _DEBUG && __has_include(<vld.h>)
+#include <vld.h>
+#endif
+
+#include "Minigin.h"
+#include "SceneManager.h"
+#include "ResourceManager.h"
+#include "TextObject.h"
+#include "TransformComponent.h"
+#include "RenderComponent.h"
+#include "TextComponent.h"
+#include "FPSComponent.h"
+#include "RotatorComponent.h"
+#include "CacheTestComponent.h"
+#include "Scene.h"
+#include "GameObjectCommand.h"
+#include "InputManager.h"
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <XInput.h>
+
+#include <filesystem>
+namespace fs = std::filesystem;
+using namespace dae;
+
+static void load()
+{
+	auto& scene = dae::SceneManager::GetInstance().CreateScene();
+
+	//background
+	auto go = std::make_unique<dae::GameObject>();
+	go->AddComponent<dae::RenderComponent>("background.png");
+	scene.Add(std::move(go));
+
+	//logo
+	go = std::make_unique<dae::GameObject>();
+	go->AddComponent<dae::RenderComponent>("logo.png");
+	go->SetLocalPosition({ 400.f,240.f,0.f });
+	scene.Add(std::move(go));
+
+	//fps counter
+	go = std::make_unique<dae::GameObject>();
+	auto font = dae::ResourceManager::GetInstance().LoadFont("Lingua.otf", 36);
+
+	// Explicitly construct the color to avoid passing a braced-init-list into make_unique
+	SDL_Color white{ 255, 255, 255, 255 };
+	auto textComp = go->AddComponent<dae::TextComponent>(font, white);  // White
+	textComp->SetText("FPS: 0");
+
+	//Add FPSComponent (updates the text)
+	go->AddComponent<FPSComponent>();
+	scene.Add(std::move(go));
+
+	//rotating diggers
+	/*auto centerWidget = std::make_unique<GameObject>();
+	centerWidget->SetLocalPosition({ 500.f, 500.f, 0.f });
+
+	auto childCharacter1 = std::make_unique<GameObject>();
+	auto childCharacter2 = std::make_unique<GameObject>();
+
+	childCharacter1->SetParent(centerWidget.get());
+	childCharacter1->AddComponent<RenderComponent>("digger2.png");
+	childCharacter1->AddComponent<RotatorComponent>(10.f, 10.f);
+
+	childCharacter2->SetParent(childCharacter1.get());
+	childCharacter2->AddComponent<RenderComponent>("digger2.png");
+	childCharacter2->AddComponent<RotatorComponent>(-5.f, 50.f);
+
+	scene.Add(std::move(centerWidget));
+	scene.Add(std::move(childCharacter1));
+	scene.Add(std::move(childCharacter2));*/
+
+	//add IMGUI Trash The Cash
+	/*auto cacheTestObj = std::make_unique<GameObject>();
+	cacheTestObj->AddComponent<CacheTestComponent>();
+	scene.Add(std::move(cacheTestObj));*/
+
+	//add move player 1
+	constexpr float speed{ 1.f };
+	auto WASDPlayer = std::make_unique<GameObject>();
+	WASDPlayer->SetLocalPosition({ 300.f,300.f,0.f });
+	GameObject* pWASDPlayer = WASDPlayer.get();
+	WASDPlayer->AddComponent<RenderComponent>("digger2.png");
+	scene.Add(std::move(WASDPlayer));
+
+	auto& input = InputManager::GetInstance();
+	//wasd
+	input.BindKeyboardCommand(SDL_SCANCODE_W, InputManager::TriggerType::Isdown
+		, std::make_unique<MoveGameObjectCommand>(
+			pKeyPlayer, speed, glm::vec3{ 0.f,-1.f,0.f }));
+
+	input.BindKeyboardCommand(SDL_SCANCODE_A, InputManager::TriggerType::Isdown
+		, std::make_unique<MoveGameObjectCommand>(
+			pKeyPlayer, speed, glm::vec3{ -1.f,0.f,0.f }));
+
+	input.BindKeyboardCommand(SDL_SCANCODE_S, InputManager::TriggerType::Isdown
+		, std::make_unique<MoveGameObjectCommand>(
+			pKeyPlayer, speed, glm::vec3{ 0.f,1.f,0.f }));
+
+	input.BindKeyboardCommand(SDL_SCANCODE_D, InputManager::TriggerType::Isdown
+		, std::make_unique<MoveGameObjectCommand>(
+			pKeyPlayer, speed, glm::vec3{ 1.f,0.f,0.f }));
+
+	//add move player2
+	auto keyBoardPlayer = std::make_unique<GameObject>();
+	keyBoardPlayer->SetLocalPosition({ 300.f,350.f,0.f });
+	GameObject* pKeyPlayer = keyBoardPlayer.get();
+	keyBoardPlayer->AddComponent<RenderComponent>("digger2.png");
+	scene.Add(std::move(keyBoardPlayer));
+
+	
+	//arrows
+	input.BindKeyboardCommand(SDL_SCANCODE_UP, InputManager::TriggerType::Isdown
+		, std::make_unique<MoveGameObjectCommand>(
+			pKeyPlayer, speed, glm::vec3{ 0.f,-1.f,0.f }));
+
+	input.BindKeyboardCommand(SDL_SCANCODE_LEFT, InputManager::TriggerType::Isdown
+		, std::make_unique<MoveGameObjectCommand>(
+			pKeyPlayer, speed, glm::vec3{ -1.f,0.f,0.f }));
+
+	input.BindKeyboardCommand(SDL_SCANCODE_DOWN, InputManager::TriggerType::Isdown
+		, std::make_unique<MoveGameObjectCommand>(
+			pKeyPlayer, speed, glm::vec3{ 0.f,1.f,0.f }));
+
+	input.BindKeyboardCommand(SDL_SCANCODE_RIGHT, InputManager::TriggerType::Isdown
+		, std::make_unique<MoveGameObjectCommand>(
+			pKeyPlayer, speed, glm::vec3{ 1.f,0.f,0.f }));
+
+
+	/*auto font = dae::ResourceManager::GetInstance().LoadFont("Lingua.otf", 36);
+	auto to = std::make_unique<dae::TextObject>("Programming 4 Assignment", font);
+	to->SetColor({ 255, 255, 0, 255 });
+	to->SetPosition(292, 20);
+	scene.Add(std::move(to));*/
+}
+
+int main(int, char* []) {
+#if __EMSCRIPTEN__
+	fs::path data_location = "";
+#else
+	fs::path data_location = "./Data/";
+	if (!fs::exists(data_location))
+		data_location = "../Data/";
+#endif
+	dae::Minigin engine(data_location);
+	engine.Run(load);
+	return 0;
+}
