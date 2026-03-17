1846eafae9a16b02cdaa9137b4bb0cae1c8f42e3
jorritDelanghe <157893404+jorritDelanghe@users.noreply.github.com>
2026-03-17 16:31:14 +0100

    Subject class
    keeps track of the observers, doesnt own, only contains all of them

---
+++ Minigin/Subject.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "Observer.h"
+#include <vector>
+#include <memory>
+namespace dae
+{
+	enum class Event;
+	class GameObject;
+	class Subject final
+	{
+	public: 
+		explicit Subject() = default;
+		~Subject() = default;
+
+		Subject(const Subject& other) = delete;
+		Subject(Subject&& other) = delete;
+		Subject& operator=(const Subject& other) = delete;
+		Subject& operator= (Subject && other) = delete;
+
+		void AddObservers(Observer* observer)
+		{
+			m_observers.push_back(observer);
+		}
+		void RemoveObserver(Observer* observer)
+		{
+			m_observers.erase(std::remove(m_observers.begin(), m_observers.end(), observer));
+		}
+	protected:
+		void NotifyObservers(Event event, GameObject* gameObject)
+		{
+			for (const auto& observer : m_observers)
+			{
+				observer->Notify(event, gameObject);
+			}
+		}
+
+	private:
+		std::vector<Observer*> m_observers{};
+	};
+
+}
