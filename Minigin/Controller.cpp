69d33d1ee3dbe096c2e704312abbd5c6c040a572
jorritDelanghe <157893404+jorritDelanghe@users.noreply.github.com>
2026-03-17 11:47:36 +0100

    typo in #define

--- Minigin/Controller.cpp
+++ Minigin/Controller.cpp
@@ -5,7 +5,7 @@
 	#include "Xinput.h" //not in the header so I get pimple, needs things in windows.h always put under windows.h
 	#pragma comment(lib,"xinput.lib")
 #else
-	#include <SDl3/SDL.h>
+	#include <SDL3/SDL.h>
 	#include <SDL3/SDL_gamepad.h>
 #endif
 namespace dae
