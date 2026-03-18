d607eee545882bc9056fc63a877735889c8c87bd
jorritDelanghe <157893404+jorritDelanghe@users.noreply.github.com>
2026-03-18 12:41:49 +0100

    USE STEAMWORKS
    implement steaamworks

--- Minigin/Minigin.cpp
+++ Minigin/Minigin.cpp
@@ -6,9 +6,14 @@
 #define WIN32_LEAN_AND_MEAN 
 #include <windows.h>
 #endif
+#if USE_STEAMWORKS
+#pragma warning (push)
+#pragma warning (disable:4996)
+#include <steam_api.h>
+#pragma warning (pop)
+#endif
 
 #include <SDL3/SDL.h>
-//#include <SDL3_image/SDL_image.h>
 #include <SDL3_ttf/SDL_ttf.h>
 #include "Minigin.h"
 #include "InputManager.h"
@@ -76,12 +81,20 @@ dae::Minigin::Minigin(const std::filesystem::path& dataPath)
 		throw std::runtime_error(std::string("SDL_CreateWindow Error: ") + SDL_GetError());
 	}
 
+#if USE_STEAMWORKS
+	if (!SteamAPI_Init())
+		throw std::runtime_error(std::string("Fatal Error - Steam must be running to play this game (SteamAPI_Init() failed)."));
+#endif
+
 	Renderer::GetInstance().Init(g_window);
 	ResourceManager::GetInstance().Init(dataPath);
 }
 
 dae::Minigin::~Minigin()
 {
+#if USE_STEAMWORKS
+	SteamAPI_Shutdown();
+#endif
 	Renderer::GetInstance().Destroy();
 	SDL_DestroyWindow(g_window);
 	g_window = nullptr;
@@ -118,6 +131,9 @@ void dae::Minigin::RunOneFrame()
 	dae::GameTime::deltaTime = deltaTime; //set deltatime as global variable
 
 	//process input
+#if USE_STEAMWORKS
+	SteamAPI_RunCallbacks();
+#endif 
 	m_quit = !InputManager::GetInstance().ProcessInput();
 
 	//update
