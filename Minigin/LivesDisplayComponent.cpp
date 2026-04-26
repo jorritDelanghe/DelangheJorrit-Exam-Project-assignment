50066543c410a649b7d4e29045d2851d69cc8266
jorritDelanghe <157893404+jorritDelanghe@users.noreply.github.com>
2026-04-26 23:27:17 +0200

    displays images of the digger now as health

--- Minigin/LivesDisplayComponent.cpp
+++ Minigin/LivesDisplayComponent.cpp
@@ -3,13 +3,22 @@
 #include "HealthComponent.h"
 #include "GameObject.h"
 #include "Event.h"
+#include "ResourceManager.h"
+#include "Renderer.h"
 #include <string>
 
-dae::LivesDisplayComponent::LivesDisplayComponent(GameObject* gameObject, std::shared_ptr<Font> font, SDL_Color color, int startLives)
+dae::LivesDisplayComponent::LivesDisplayComponent(GameObject* gameObject, const std::string& fileName, float size, float spacing, int startLives)
 	:Component(gameObject)
+	,m_size(size)
+	,m_spacing(spacing)
+	, m_lives(startLives)
 {
-	m_text = gameObject->AddComponent<TextComponent>(font, color);
-	m_text->SetText("Lives: " + std::to_string(startLives));
+	m_texture = ResourceManager::GetInstance().LoadTexture(fileName);
+}
+
+void dae::LivesDisplayComponent::Render() const
+{
+	DrawLives();
 }
 
 void dae::LivesDisplayComponent::Notify(GameEvent event, GameObject* gameObject)
@@ -17,9 +26,20 @@ void dae::LivesDisplayComponent::Notify(GameEvent event, GameObject* gameObject)
 	if (event == GameEvent::PlayerDied)
 	{
 		auto* health = gameObject->GetComponent<HealthComponent>();
-		if (health && m_text)
+		if (health)
 		{
-			m_text->SetText("Lives: " + std::to_string(health->GetLives()));
+			m_lives = health->GetLives();
 		}
+	}
+}
+
+void dae::LivesDisplayComponent::DrawLives() const
+{
+	const auto pos{GetOwner()->GetLocalPosition()};
+
+	for (int i{}; i < m_lives;++i)
+	{
+		const float x =pos.x + (m_spacing + m_size) * static_cast<float>(i);
+		Renderer::GetInstance().RenderTexture(*m_texture,x,pos.y,m_size,m_size);
 	}
 }
--- Minigin/LivesDisplayComponent.h
+++ Minigin/LivesDisplayComponent.h
@@ -3,6 +3,7 @@
 #include "Observer.h"
 #include <memory>
 #include <SDL3/SDL.h>  
+#include "Texture2D.h"
 
 namespace dae
 {
@@ -14,7 +15,7 @@ namespace dae
 	class LivesDisplayComponent final : public Component, public Observer
 	{
 	public:
-		explicit LivesDisplayComponent(GameObject* gameObject, std::shared_ptr<Font> font, SDL_Color color, int startLives);
+		explicit LivesDisplayComponent(GameObject* gameObject, const std::string& fileName, float size, float spacing, int startLives);
 		virtual ~LivesDisplayComponent() noexcept override = default;
 
 		LivesDisplayComponent(const LivesDisplayComponent& other) = delete;
@@ -22,9 +23,15 @@ namespace dae
 		LivesDisplayComponent& operator=(const LivesDisplayComponent& other) = delete;
 		LivesDisplayComponent& operator= (LivesDisplayComponent&& other) = delete;
 
+		virtual void Render() const override;
 		virtual void Notify(GameEvent event, GameObject* gameObject) override;
 	private:
-		TextComponent* m_text{nullptr};
+		void DrawLives() const;
+
+		std::shared_ptr<Texture2D>m_texture;
+		float m_size{};
+		float m_spacing{};
+		int m_lives{};
 	};
 
 }
