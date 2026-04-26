9e9432c8f8ad165b4e27ca924ecde3b16c26392c
jorritDelanghe <157893404+jorritDelanghe@users.noreply.github.com>
2026-04-26 22:57:40 +0200

    display lives and points

--- Digger/DiggerScene.cpp
+++ Digger/DiggerScene.cpp
@@ -11,6 +11,12 @@
 
 #include "GridComponent.h"
 #include "GameObject.h"
+
+#include "LivesDisplayComponent.h"
+#include "PointsDisplayComponent.h"
+#include "ResourceManager.h"
+#include "PointsComponent.h"
+#include "HealthComponent.h"
 namespace dae
 {
 	void diggerScene::loadScene()
@@ -20,16 +26,39 @@ namespace dae
 
 		auto gridObject = std::make_unique<GameObject>(); //first create the gameobject
 		gridObject->AddComponent<GridComponent>("Data/Resources/Level01.txt"); //then add the grid component to it
+		gridObject->SetLocalPosition({ 0.f,40.f,0.f });
 		auto* rawPtrGrid{ gridObject->GetComponent<GridComponent>()};
 		scene.Add(std::move(gridObject)); //then add the gameobject to the scene	
 
 		//player
+		constexpr int numLives{ 3 };
 		auto diggerPlayer{ std::make_unique<GameObject>() };
 		diggerPlayer->AddComponent<RenderComponent>("digger2.png");
+		auto* points = diggerPlayer->AddComponent<PointsComponent>();
+		auto* health = diggerPlayer->AddComponent<HealthComponent>(numLives);
 		diggerPlayer->SetLocalPosition({ 100.f,100.f,0.f });
 
 		auto* diggerPlayerRawPtr{ diggerPlayer.get() };
 		scene.Add(std::move(diggerPlayer));
+
+		// lives display object
+		auto font = dae::ResourceManager::GetInstance().LoadFont("Resources/GameFont.ttf", 36);
+		auto livesDisplayObj = std::make_unique<GameObject>();
+		auto* livesDisplay = livesDisplayObj->AddComponent<LivesDisplayComponent>(font, SDL_Color{ 255,255,255,255 },3);
+		livesDisplayObj->SetLocalPosition({ 100.f, 0.f, 0.f });
+		scene.Add(std::move(livesDisplayObj));
+	
+		//points display
+		constexpr SDL_Color green{ 0,255,0,255 };
+		auto pointsDisplayObj = std::make_unique<GameObject>();
+		auto* pointsDisplay = pointsDisplayObj->AddComponent<PointsDisplayComponent>(font, green);
+		pointsDisplayObj->SetLocalPosition({ 10.f, 0.f, 0.f });
+		scene.Add(std::move(pointsDisplayObj));
+	
+		//observers
+		health->OnDied().AddObservers(livesDisplay);
+		points->OnPointsChanged().AddObservers(pointsDisplay);
+	
 
 		//controls keyboard
 		auto& input = InputManager::GetInstance();
@@ -52,6 +81,28 @@ namespace dae
 			(diggerPlayerRawPtr, speed, glm::vec3{ 0.f,1.f,0.f }, rawPtrGrid));
 
 		input.BindKeyboardCommand(SDL_SCANCODE_D
+			, InputManager::TriggerType::Isdown
+			, std::make_unique<MoveGameObjectCommand>
+			(diggerPlayerRawPtr, speed, glm::vec3{ 1.f,0.f,0.f }, rawPtrGrid));
+
+		//arrows
+
+		input.BindKeyboardCommand(SDL_SCANCODE_UP
+			, InputManager::TriggerType::Isdown
+			, std::make_unique<MoveGameObjectCommand>(
+				diggerPlayerRawPtr, speed, glm::vec3{ 0.f,-1.f,0.f }, rawPtrGrid));
+
+		input.BindKeyboardCommand(SDL_SCANCODE_LEFT
+			, InputManager::TriggerType::Isdown
+			, std::make_unique<MoveGameObjectCommand>
+			(diggerPlayerRawPtr, speed, glm::vec3{ -1.f,0.f,0.f }, rawPtrGrid));
+
+		input.BindKeyboardCommand(SDL_SCANCODE_DOWN
+			, InputManager::TriggerType::Isdown
+			, std::make_unique<MoveGameObjectCommand>
+			(diggerPlayerRawPtr, speed, glm::vec3{ 0.f,1.f,0.f }, rawPtrGrid));
+
+		input.BindKeyboardCommand(SDL_SCANCODE_RIGHT
 			, InputManager::TriggerType::Isdown
 			, std::make_unique<MoveGameObjectCommand>
 			(diggerPlayerRawPtr, speed, glm::vec3{ 1.f,0.f,0.f }, rawPtrGrid));
