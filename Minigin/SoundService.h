3152c3cf290b5091abd890440634857ce903781a
jorritDelanghe <157893404+jorritDelanghe@users.noreply.github.com>
2026-04-28 20:22:35 +0200

    SoundService class

--- Minigin/SoundService.h
+++ Minigin/SoundService.h
@@ -1,24 +1,35 @@
 #pragma once
-
+#include <string>
 
 //interface
 namespace dae
 {
+	using SoundID = unsigned short; //never more  then 65,535 sounds
 	class SoundService
 	{
 	public:
-		SoundService();
-		virtual ~SoundService();
+		SoundService() = default; // not necessary constructor is default constructed
+		virtual ~SoundService() = default;
 
+		//rule of 5 not really need because abstract class needs to be inherited from
 		SoundService(const SoundService& other) =delete;
 		SoundService( SoundService&& other) =delete;
 		SoundService& operator=(const SoundService& other) =delete;
 		SoundService& operator=(SoundService&& other) =delete;
 
-		virtual void Play();
-		virtual void Stop();
-		virtual void Pause();
+		virtual void Play(SoundID soundID, float volume) = 0;
+		virtual void Stop(SoundID sounID) = 0;
+		virtual SoundID AddSound(const std::string& soundPathName) = 0;
 
+	};
+
+	//loactor can never be a nullptr 
+	class NullSoundService final : public SoundService
+	{
+	public:
+		virtual void Play(SoundID soundID, float volume) override {}
+		virtual void Stop(SoundID sounID) override {}
+		virtual SoundID AddSound(const std::string& soundPathName) override {return 0;}
 	};
 
 }
