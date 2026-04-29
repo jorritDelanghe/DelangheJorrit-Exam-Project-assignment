bbd6348bdb2fb7d2c90da3de267485c33a489919
jorritDelanghe <157893404+jorritDelanghe@users.noreply.github.com>
2026-04-29 14:10:39 +0200

    return by value

--- Minigin/ServiceLocator.h
+++ Minigin/ServiceLocator.h
@@ -1,26 +1,23 @@
 #pragma once
-
 #include "SoundService.h"
 #include <memory>
+
 namespace dae
 {
-	class ServiceLoctor final
-	{
-	public:
-		static SoundService GetSoundSystem() const { return m_soundSystem; }
-		static RegisterSounSystem(std::unique_ptr<SoundService> soundSystem)
-		{
-			if (soundSystem)
-			{
-				m_soundSystem = std::move(soundSystem)
-			}
-			else
-			{
-				m_soundSystem = std::make_unique<NullSoundService>();
-			}
+    class ServiceLocator final
+    {
+    public:
+        static SoundService& GetSoundSystem() { return *m_soundSystem; }
 
-		}
-	private:
-		static std::unique_ptr<SoundService> m_soundSystem;
-	}
+        static void RegisterSounSystem(std::unique_ptr<SoundService> soundSystem)
+        {
+            if (soundSystem)
+                m_soundSystem = std::move(soundSystem);
+            else
+                m_soundSystem = std::make_unique<NullSoundService>();
+        }
+
+    private:
+        static std::unique_ptr<SoundService> m_soundSystem;
+    };
 }
