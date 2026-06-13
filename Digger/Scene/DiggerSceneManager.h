d691b7a38d4a5790fd876930af9c06b0070eb47e
jorritDelanghe <157893404+jorritDelanghe@users.noreply.github.com>
2026-06-13 11:00:59 +0200

    cleaned up diggermanager

--- Digger/Scene/DiggerSceneManager.h
+++ Digger/Scene/DiggerSceneManager.h
@@ -21,25 +21,15 @@ namespace dae
 		DiggerSceneManager& operator=(DiggerSceneManager&& other) = delete;
 
 		void LoadScreen();
-		int GetScore()const { return m_currentScore; }
-		int GetLives()const { return m_currentLives; }
 		void SetState(std::unique_ptr<GameState> newState); //needed in states
 
 		//next level observer
 		virtual void Notify(GameEvent event, GameObject* gameObject) override;
 	private:
-
-		void LoadDiggerLevel(const LevelData& levelData);
 		void InitSound() const;
 		void InitInput();
-		//save
-		void SaveCurrentGameData();
-
-		int m_currentScore{};
-		int m_currentLives{4};
 
 		GameObject* m_currentPlayer{ nullptr }; //need to be a pointer because it doesnt own
-
 		std::unique_ptr<GameState> m_currentGameState{nullptr};
 
 
